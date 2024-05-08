2ae51d212d3215cd9b5bdb02320219821a575f87
도운 <dbsehdns12@gmail.com>
2024-05-08 15:40:58 +0900

    Feat : Navigation bar 추가

--- app/src/main/java/com/example/pathfinder/MainActivity.java
+++ app/src/main/java/com/example/pathfinder/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.pathfinder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.pathfinder.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.startFragment) {
+                binding.bottomNavigationView.setVisibility(View.GONE);
+            } else {
+                binding.bottomNavigationView.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pathfinder/StartFragment.java
+++ app/src/main/java/com/example/pathfinder/StartFragment.java
@@ -1,5 +1,6 @@
 package com.example.pathfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -57,10 +58,19 @@ public class StartFragment extends Fragment {
         UserApiClient.getInstance().me((user, throwable) -> {
             if (user != null) {
                 Log.println(Log.INFO, "SignUpFragment", "사용자 정보 요청 성공");
+                openMainActivity();
             } else {
                 Log.e("SignUpFragment", "사용자 정보 요청 실패", throwable);
             }
             return null;
         });
     }
+
+    private void openMainActivity() {
+        if (getActivity() != null) {
+            getActivity().finish(); // 회원가입 프래그먼트를 호스팅하는 액티비티를 종료합니다.
+            startActivity(new Intent(getActivity(), MainActivity.class));
+        }
+    }
+
 }
