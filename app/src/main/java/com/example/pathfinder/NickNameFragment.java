97dff1a509f6465f7e7a2a987ebc20bcf29b50bf
도운 <dbsehdns12@gmail.com>
2024-05-13 11:16:25 +0900

    Feat : User Name 가져오기

--- app/src/main/java/com/example/pathfinder/NickNameFragment.java
+++ app/src/main/java/com/example/pathfinder/NickNameFragment.java
@@ -1,22 +1,31 @@
 package com.example.pathfinder;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.pathfinder.databinding.FragmentNickNameBinding;
 
 public class NickNameFragment extends Fragment {
+    private FragmentNickNameBinding binding;
+    private SharedViewModel viewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FragmentNickNameBinding binding = FragmentNickNameBinding.inflate(inflater, container, false);
+        binding = FragmentNickNameBinding.inflate(inflater, container, false);
+        viewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        viewModel.getNickname().observe(getViewLifecycleOwner(), nickname -> {
+            // 여기서 닉네임을 사용하세요
+            binding.etNickName.setText(nickname);
+        });
+
         return binding.getRoot();
     }
 }
