cfab66d01c844ebe0e9f6ed660e892d4f7d49119
kangdayeong <kangdy7599@gmail.com>
2024-05-29 11:30:03 +0900

    Feat : navigation 연결

--- app/src/main/java/com/example/pathfinder/ui/fragment/PeriodFragment.java
+++ app/src/main/java/com/example/pathfinder/ui/fragment/PeriodFragment.java
@@ -8,16 +8,26 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
+import com.example.pathfinder.R;
 import com.example.pathfinder.databinding.FragmentPeriodBinding;
 
 public class PeriodFragment extends Fragment {
-    private FragmentPeriodBinding binding;
 
+    private FragmentPeriodBinding binding;
+    private NavController navController;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentPeriodBinding.inflate(inflater, container, false);
+
+
+        binding.btnNext.setOnClickListener(v -> {
+            navController.navigate(R.id.action_periodFragment_to_selectWhoFragment);
+        });
+
+
         return binding.getRoot();
     }
 }
